09e8da9c68d0aa4c672a0685c02cb35fe3c32d92
Tommy Jebb <tjebb@steadfast.com>
2015-12-14 20:01:38 -0500

    leaving work!

--- app/models/golfer.js
+++ app/models/golfer.js
@@ -1,15 +1,15 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  firstName: DS.attr('string'),
-  lastName: DS.attr('string'),
-  ranking: DS.attr('number'),
-  country: DS.attr('string'),
-  score: DS.attr('number'),
-  owners: DS.hasMany('user'),
+	firstName: DS.attr('string'),
+	lastName: DS.attr('string'),
+	ranking: DS.attr('number'),
+	country: DS.attr('string'),
+	score: DS.attr('number'),
+	owners: DS.hasMany('user'),
 
-  name: Ember.computed('firstName', 'lastName', function() {
-  	return `${this.get('firstName')} ${this.get('lastName')}`;
-  })
+	name: Ember.computed('firstName', 'lastName', function() {
+		return `${this.get('firstName')} ${this.get('lastName')}`;
+	})
   
 });
