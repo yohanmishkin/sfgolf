75c97d9f5b7daebd6af5010ebf0b77f3b3b758cf
Tommy Jebb <tjebb@steadfast.com>
2016-01-08 13:09:19 -0500

    Starting golfer ranking

--- app/mirage/factories/golfer.js
+++ app/mirage/factories/golfer.js
@@ -3,7 +3,7 @@ import Mirage, {faker} from 'ember-cli-mirage';
 export default Mirage.Factory.extend({
   firstName: faker.name.firstName,
   lastName: faker.name.lastName,
-  ranking: faker.random.number,
+  ranking: function(i) { return i + 1; },
   country: faker.address.country,
   age: faker.random.number,
   teams: []
