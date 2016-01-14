20330e0404d13c2de9f6ab6d800472be49da7e7a
Yohan Mishkin <yohanmishkin@protonmail.com>
2016-01-14 00:32:38 -0500

    Adding round specific scores

--- app/models/golfer.js
+++ app/models/golfer.js
@@ -5,8 +5,13 @@ export default DS.Model.extend({
 	lastName: DS.attr('string'),
 	ranking: DS.attr('number'),
 	country: DS.attr('string'),
-	score: DS.attr('number'),
 	teams: DS.hasMany('team'),
+
+	r1: DS.attr('number'),
+	r2: DS.attr('number'),
+	r3: DS.attr('number'),
+	r4: DS.attr('number'),
+	score: Ember.computed.sum('r1', 'r2', 'r3', 'r4'),
 
 	name: Ember.computed('firstName', 'lastName', function() {
 		return `${this.get('firstName')} ${this.get('lastName')}`;
