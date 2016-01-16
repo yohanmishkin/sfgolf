30618112e7d01b76e88c40edc5ba3a5eebb3397b
Tommy Jebb <tjebb@steadfast.com>
2016-01-15 20:05:11 -0500

    More default data

---
+++ app/mirage/fixtures/golfers.js
@@ -0,0 +1,18 @@
+
+var names = ('Kevin Kisner','Zach Johnson','Scott Piercy','Daniel Summerhays','James Hahn',
+	'Francesco Molinari','Marc Leishman','John enden','Harris English',	'Charles Howell',
+	'Si Woo','Shawn Stefani','Greg Owen','Jason Dufner','Webb Simpson',	'Tony Finau',
+	'Ricky Barnes','Fred Funk','Shane Bertsch','Ryan Palmer',	'Vijay Singh',
+	'Luke Donald','Jerry Kelly','David Hearn','Zac lair','Daisuke Kataoka',	'Hideto Tanihara',
+	'Cameron Smith','Daniel Berger','Lucas Glover','Brendon de','Stewart Cink',	'David ingmerth',
+	'Davis Love','Kevin Na','Colt Knost','Derek Fathauer','Steven Bowditch', 'Padraig Harrington',
+	'Danny Lee','Tim Clark','Morgan offmann','Gary Woodland','Bronson Burgoon',	'Jamie Lovemark',
+	'Meen Whee','Ryo Ishikawa','Michael Thompson','Chad Campbell','Spencer evin', 'Pat Perez',
+	'Nick Taylor','K.J','Jimmy Walker','Jim Herman','Tyrone van','Kyle Reifers', 'Nick Mason',
+	'Harold arner','Johnson Wagner','Steve Stricker','Seung-yul','Emiliano Grillo','Adam Scott','Chez Reavie');
+
+export default [
+	{ id: 1, firstName: 'Rob', lastName: 'Pitts' },
+	{ id: 2, firstName: 'Doug', lastName: 'Miller' },
+	{ id: 3, firstName: 'Miraj', lastName: 'Patel' },
+];
--- app/mirage/scenarios/default.js
+++ app/mirage/scenarios/default.js
@@ -4,13 +4,24 @@ export default function( server ) {
 	var rob = server.create('user', { firstName: 'Rob', lastName: 'Pitts'});
 	var doug = server.create('user', { firstName: 'Doug', lastName: 'Miller'});
 	var miraj = server.create('user', { firstName: 'Miraj', lastName: 'Patel'});
+	var javier = server.create('user', { firstName: 'Javier', lastName: 'Benitez' });
+	var emily = server.create('user', { firstName: 'Emily', lastName: 'Fung' });
+	var blake = server.create('user', { firstName: 'Blake', lastName: 'Myers' });
+	var pete = server.create('user', { firstName: 'Pete', lastName: 'Treadway' });
 
 	var team1 = server.create('team', {user: rob.id});
 	var team2 = server.create('team', { user: doug.id });
 	var team3 = server.create('team', { user: miraj.id });
+	var team4 = server.create('team', { user: javier.id });
+	var team5 = server.create('team', { user: emily.id });
+	var team6 = server.create('team', { user: blake.id });
+	var team7 = server.create('team', { user: pete.id });
 
-	server.createList('golfer', 5, { teams: [team1.id] } );
-	server.createList('golfer', 7, { teams: [team2.id] } );
+	server.createList('golfer', 10, { teams: [team1.id, team6.id, team7.id] } );
+	server.createList('golfer', 10, { teams: [team2.id, team5.id] } );
+	server.createList('golfer', 10, { teams: [team3.id, team4.id] } );
+	
+
 	// server.createList('golfer', 30);
 	
 	// var team1 = server.create('team', { id: 1 });
