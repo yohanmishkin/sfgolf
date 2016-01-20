70242a27c556c6d8d0d1c35d2c998a81b3ee0fa1
Tommy Jebb <tjebb@steadfast.com>
2016-01-20 13:05:59 -0500

    Adding in registration and authentication.

--- app/routes/golfers.js
+++ app/routes/golfers.js
@@ -3,7 +3,7 @@ import Golfer from '../models/golfer';
 
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Ember.Route.extend(AuthenticatedRouteMixin, {
 	model: function() {
 		return this.store.findAll('golfer');
 	}
